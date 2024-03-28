ddf2aa3a23a50680ad4dc29c8dc8b707d0970994
madebyparry <nicholas@madebyparry.com>
2024-03-27 20:59:27 -0400

    Neovim compiliation update - universal paths

--- update_nvim_cmp.sh
+++ update_nvim_cmp.sh
@@ -1,11 +1,42 @@
+echo
+echo "-------------------------------------------"
+echo
+echo "removing neovim from apt"
+echo
+echo "-------------------------------------------"
+echo
 sudo apt remove neovim
-wget https://github.com/neovim/neovim/archive/refs/tags/stable.zip
+echo
+echo "-------------------------------------------"
+echo
+echo "downloading latest neovim stable"
+echo
+echo "-------------------------------------------"
+echo
+wget -O https://github.com/neovim/neovim/archive/refs/tags/stable.zip ~/.config/neovim/stable.zip
 unzip stable.zip
 cd neovim-stable/
+echo
+echo "-------------------------------------------"
+echo
+echo "compiling neovim"
+echo
+echo "-------------------------------------------"
+echo
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-mv neovim-stable/build/bin/nvim /opt/nvim
-sudo mv neovim-stable/build/bin/nvim /opt/nvim
+echo
+echo "-------------------------------------------"
+echo
+echo "compiled neovim so neovim-stable/build/bin"
+echo
+echo "-------------------------------------------"
+echo
+echo "moving to /opt/nvim"
+echo
+echo "-------------------------------------------"
+echo
+sudo mv ~/.config/nvim/neovim-stable/build/bin/nvim /opt/nvim
 #cd ../
 #build/bin/nvim
 #ls build/bin/
