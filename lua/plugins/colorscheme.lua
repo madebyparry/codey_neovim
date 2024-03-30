2468d3f2bf5f959d2d298e128e023834f6df2a3a
madebyparry <nicholas@madebyparry.com>
2024-03-29 20:47:55 -0400

    Lua transition - custom lazyvim WIP

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "mcchrish/zenbones.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+}
