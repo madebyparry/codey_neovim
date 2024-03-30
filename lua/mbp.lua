19f4ca0100fbe674936e6c987b20a2b4301d09d5
MadeByParry <nicholas@madebyparry.com>
2024-03-30 20:55:17 +0000

    finally it is becomming my own

--- lua/mbp.lua
+++ lua/mbp.lua
@@ -1,7 +1,10 @@
 --  Colors Setup 
 require 'nordic' .setup {
-  transparent_bg = true,
+  -- transparent_bg = true,
+  bright_border = false,
+  swap_backgrounds = false,
   bold_keywords = true
 }
 require 'nordic' .load()
 
+vim.opt.guifont='Courier New'
